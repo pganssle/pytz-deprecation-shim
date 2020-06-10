cb1a84e9f76f786d9d8b27d867972d13b4d5fc9c
Paul Ganssle <paul@ganssle.io>
2020-06-10 12:07:57 -0400

    Test that ValueError is raised for localize/normalize

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 import hypothesis
 import pytest
 import pytz
 
 import pytz_deprecation_shim as pds
 
-from ._common import invalid_zone_strategy
+from ._common import invalid_zone_strategy, valid_zone_strategy
 
 
 @hypothesis.given(key=invalid_zone_strategy)
@@ -14,3 +16,21 @@ def test_invalid_zones(key):
         pds.timezone(key)
 
     assert issubclass(exc_info.type, pytz.UnknownTimeZoneError)
+
+
+@hypothesis.given(key=valid_zone_strategy)
+def test_localize_aware_datetime(key):
+    zone = pds.timezone(key)
+    dt = datetime(2020, 1, 1, tzinfo=pds.UTC)
+
+    with pytest.warns(pds.PytzUsageWarning), pytest.raises(ValueError):
+        zone.localize(dt)
+
+
+@hypothesis.given(key=valid_zone_strategy)
+def test_normalize_naive_datetime(key):
+    zone = pds.timezone(key)
+    dt = datetime(2020, 1, 1)
+
+    with pytest.warns(pds.PytzUsageWarning), pytest.raises(ValueError):
+        zone.normalize(dt)
