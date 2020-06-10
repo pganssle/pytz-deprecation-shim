e4adb1459f93d3a04a73197ebf4240c678e02360
Paul Ganssle <paul@ganssle.io>
2020-06-10 16:25:31 -0400

    Adjust hypothesis CI profile

--- conftest.py
+++ conftest.py
@@ -1,8 +1,11 @@
 import os
+from datetime import timedelta
 
 import hypothesis
 
 hypothesis.settings.register_profile("long", max_examples=5000)
-hypothesis.settings.register_profile("ci", max_examples=20000)
+hypothesis.settings.register_profile(
+    "ci", max_examples=2000, deadline=timedelta(seconds=1)
+)
 
 hypothesis.settings.load_profile(os.getenv(u"HYPOTHESIS_PROFILE", "default"))
