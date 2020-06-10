59dd9442aed2cfa32208c11ba98a085ba86b2b82
Paul Ganssle <paul@ganssle.io>
2020-06-10 11:39:57 -0400

    Add CI profile for hypothesis

--- conftest.py
+++ conftest.py
@@ -3,5 +3,6 @@ import os
 import hypothesis
 
 hypothesis.settings.register_profile("long", max_examples=5000)
+hypothesis.settings.register_profile("ci", max_examples=20000)
 
 hypothesis.settings.load_profile(os.getenv(u"HYPOTHESIS_PROFILE", "default"))
